37cee38290c0f61421c4768ca0f0d05d5922de33
Fahir Lael <fs.lael@gmail.com>
2026-06-05 12:02:17 -0300

    22 refatorar movimentação e animação do player e do navio (#46)
    * implementação de máquina de estados para move do player, barco e capitão(ainda com bugs).
    
    * Movimentação funcionando com v1 do State Machine

---
+++ Assets/Project/Scripts/Entities/Controllers/CaptainState.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class CaptainState : PlayerMovement.IPlayerState
+{
+    private PlayerMovement player;
+
+    public CaptainState(PlayerMovement player)
+    {
+        this.player = player;
+    }
+
+    public void Enter()
+    {
+        player.crb.linearVelocity = Vector2.zero;
+    }
+
+    public void Update()
+    {
+        player.AtualizarAnimacoes();
+    }
+
+    public void FixedUpdate()
+    {
+        Vector3 currentPos = player.capitão.transform.position;
+        Tile tile = player.worldGenerator.GetTileAtWorldPosition(currentPos);
+
+        if (tile == null) return;
+
+        Vector2 direction = player.moveInput.sqrMagnitude > 1
+            ? player.moveInput.normalized
+            : player.moveInput;
+
+        if (tile.Metadata.Layer != 0)
+        {
+            player.crb.linearVelocity = direction * player.captainSpeed;
+            player.rb.linearVelocity = Vector2.zero;
+        }
+        else
+        {
+            player.crb.linearVelocity *= -1;
+        }
+    }
+
+    public void Exit() {}
+}
